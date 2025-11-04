81f4d83ec63f14c503b3e4723a9dd8661304a389
Tiago <tiago.polemarxgroup@gmail.com>
2025-11-04 14:53:01 -0300

    first commit

---
+++ src/test/java/br/com/api/anime/AnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.anime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
